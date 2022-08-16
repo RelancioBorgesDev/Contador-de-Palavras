5005e93718f7952a7a7f60eeca27ce4dd5677ef7
Relancio Borges <relancio.dev@gmail.com>
2022-08-16 01:40:44 -0300

    add: Readme

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let txtArea = document.getElementById("txt");
-let numPalavras = document.querySelector(".qntPalavras");
-let numLetras = document.querySelector(".qntLetras");
+const txtArea = document.getElementById("txt");
+const numPalavras = document.querySelector(".qntPalavras");
+const numLetras = document.querySelector(".qntLetras");
 
-console.log(numPalavras);
 
 txtArea.addEventListener("keyup", (e) => {
-    console.log(e.target.value)
     let frase = e.target.value
     let qntPalavras = frase.split(" ").filter((item) =>  /\S/.test(item));
     let qntLetras = frase.split("").filter((item) => /\S/.test(item))
